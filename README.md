f0001146ba15522d954eabf0e21ba33aa35e8150
herbaa <herbaasc@gmail.com>
2024-06-10 15:33:11 +0400

    addBadges

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Herbaa/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Herbaa/frontend-project-46/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/5132169cb8de2710fc27/maintainability)](https://codeclimate.com/github/Herbaa/frontend-project-46/maintainability)
+[![hexlet-check](https://github.com/Herbaa/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Herbaa/frontend-project-46/actions/workflows/hexlet-check.yml)
--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,24 +1,22 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
 import genDiff from "../src/index.js";
-import test from "node:test";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const getFixturePath = (filePath) => path.join(__dirname, '..', '__fixtures__', filePath);
+const file1 = getFixturePath('file1.json');
+const file2 = getFixturePath('file2.json');
 
-const fileData = (filePath) => fs.readFileSync(getFixturePath(filePath), 'utf-8');
+const answer1 = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
 
-// tests for flat files
-
-const referenceFlatFiles = fileData('flat-files-test.txt');
-
-test('Flat JSON', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-
-  expect(genDiff(file1, file2)).toEqual(referenceFlatFiles);
-})
+test('gendiff json files', () => {
+  expect(genDiff(file1, file2)).toEqual(answer1);
+});
